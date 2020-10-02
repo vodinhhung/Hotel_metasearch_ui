869a2454b2ff4093034f5d146ae05ed0e08ae7c7
Truonghdpk <truongondaihoc@gmail.com>
2020-10-02 19:18:49 +0700

    add stack follow

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import "react-native-gesture-handler";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 import React from "react";
-
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomePage from "./src/navigation/HomePage";
+import LoginFollow from "./src/navigation/Login";
 import Account from "./src/navigation/Account";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
+function AppFollow() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomePage} />
+      <Tab.Screen name="Account" component={Account} />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={HomePage} />
-          <Tab.Screen name="Account" component={Account} />
-        </Tab.Navigator>
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Login" component={LoginFollow} />
+          <Stack.Screen name="App" component={AppFollow} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
---
+++ src/navigation/Login.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+const LoginFollow = () => {
+  return <Text>Login</Text>;
+};
+export default LoginFollow;
