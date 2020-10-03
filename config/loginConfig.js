b255ac425191387f1f1ac502a33a1c76e24270b2
Truonghdpk <truongondaihoc@gmail.com>
2020-10-03 15:36:52 +0700

    init login follow

---
+++ config/loginConfig.js
@@ -0,0 +1,10 @@
+export const googleLoginConfig = {
+  clientId: '942559879619-uf0u6md7rkugseoq4csgedsu82osndn0.apps.googleusercontent.com',
+  // iosClientId: `<YOUR_IOS_CLIENT_ID_FOR_EXPO>`,
+  // androidClientId: `<YOUR_ANDROID_CLIENT_ID_FOR_EXPO>`,
+  // iosStandaloneAppClientId: `<YOUR_IOS_CLIENT_ID>`,
+  // androidStandaloneAppClientId: `<YOUR_ANDROID_CLIENT_ID>`,
+};
+export const facebookLoginConfig = {
+  appId: "2649238258738835",
+};
--- src/navigation/Login.js
+++ src/navigation/Login.js
@@ -1,6 +1,143 @@
-import React from "react";
-import { Text } from "react-native";
-const LoginFollow = () => {
-  return <Text>Login</Text>;
-};
-export default LoginFollow;
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import * as Facebook from "expo-facebook";
+import * as Google from "expo-google-app-auth";
+import {
+  facebookLoginConfig,
+  googleLoginConfig,
+} from "../../config/loginConfig";
+
+console.disableYellowBox = true;
+
+export default function App() {
+  const [isLoggedin, setLoggedinStatus] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const [isImageLoading, setImageLoadStatus] = useState(false);
+  const googleLogin = async () => {
+    const { type, accessToken, user } = await Google.logInAsync(
+      googleLoginConfig
+    );
+    if (type === "success") {
+      // Then you can use the Google REST API
+      let userInfoResponse = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        }
+      );
+    }
+  };
+  const facebookLogIn = async () => {
+    try {
+      const {
+        type,
+        token,
+        expires,
+        permissions,
+        declinedPermissions,
+      } = await Facebook.logInWithReadPermissionsAsync({
+        permissions: ["public_profile"],
+      });
+      if (type === "success") {
+        // Get the user's name using Facebook's Graph API
+        fetch(
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,picture.height(500)`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            setLoggedinStatus(true);
+            setUserData(data);
+          })
+          .catch((e) => console.log(e));
+      } else {
+        // type === 'cancel'
+      }
+    } catch ({ message }) {
+      alert(`Facebook Login Error: ${message}`);
+    }
+  };
+
+  const logout = () => {
+    setLoggedinStatus(false);
+    setUserData(null);
+    setImageLoadStatus(false);
+  };
+
+  return isLoggedin ? (
+    userData ? (
+      <View style={styles.container}>
+        <Image
+          style={{ width: 200, height: 200, borderRadius: 50 }}
+          source={{ uri: userData.picture.data.url }}
+          onLoadEnd={() => setImageLoadStatus(true)}
+        />
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          animating={!isImageLoading}
+          style={{ position: "absolute" }}
+        />
+        <Text style={{ fontSize: 22, marginVertical: 10 }}>
+          Hi {userData.name}!
+        </Text>
+        <TouchableOpacity style={styles.logoutBtn} onPress={this.logout}>
+          <Text style={{ color: "#fff" }}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+    ) : null
+  ) : (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <TouchableOpacity
+        style={styles.loginFacebookBtn}
+        onPress={facebookLogIn}
+      >
+        <Text style={{ color: "#fff" }}>Login with Facebook</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.loginGoogleBtn}
+        onPress={googleLogin}
+      >
+        <Text style={{ color: "#fff" }}>Login with Google</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e9ebee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  loginFacebookBtn: {
+    marginBottom: 10,
+    backgroundColor: "#4267b2",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  loginGoogleBtn: {
+    marginBottom: 10,
+    backgroundColor: "#E2402B",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  logoutBtn: {
+    backgroundColor: "grey",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    position: "absolute",
+    bottom: 0,
+  },
+});
