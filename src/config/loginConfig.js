ef226b9b31148f31ffede20f10b0cc75c359c2c4
Truonghdpk <truongondaihoc@gmail.com>
2020-11-28 01:43:12 +0700

    change app

--- src/config/loginConfig.js
+++ src/config/loginConfig.js
@@ -1,8 +1,8 @@
 export const googleLoginConfig = {
   iosClientId: `942559879619-8ok9ub6r8fldfh8vahtndrm270gia59q.apps.googleusercontent.com`,
-  androidClientId: `942559879619-ptdpaujllfc8aqcns2p19l5s8rkge9cq.apps.googleusercontent.com`,
+  androidClientId: `942559879619-2hhr2ma0tdmcv1hac76q1n4fcdmakr59.apps.googleusercontent.com`,
   iosStandaloneAppClientId: `942559879619-8ok9ub6r8fldfh8vahtndrm270gia59q.apps.googleusercontent.com`,
-  androidStandaloneAppClientId: `942559879619-ptdpaujllfc8aqcns2p19l5s8rkge9cq.apps.googleusercontent.com`,
+  androidStandaloneAppClientId: `942559879619-2hhr2ma0tdmcv1hac76q1n4fcdmakr59.apps.googleusercontent.com`,
   scopes: ['profile', 'email'],
   redirectUrl: "host.cff.exponent:/oauth2redirect/google"
 };
